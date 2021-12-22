d4b9767e283c039d4cebce65e9d97681b02c2750
TheEEs <visualbasic2013@hotmail.com>
2021-12-22 17:41:52 +0700

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-orange-400 w-screen flex-wrap h-screen flex justify-center">
+    <div></div>
+    <div class="bg-white shadow-lg self-end sm:max-w-lg w-full py-10 h-3/5 sm:h-2/5 rounded-t-2xl px-6">
+      <h2 class="text-3xl font-semibold text-coolGray-500 font-sans border-l-4 border-orange-400 pl-3 inline">
+        Thao tác với các bit
+      </h2>
+      <section class="mt-6">
+        <p class="font-mono text-coolGray-700">Các phép bitwise là các phép toán giúp lập trình viên thao tác với các bit dữ liệu.</p>
+        <ul class="ml-4 list-disc font-mono mt-2">
+          <li>
+            <p>Nhấn vào từng bit để đảo giá trị của bit đó</p>
+          </li>
+        </ul>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    
+  },
+  data(){
+    return {
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    }
+  }
+};
+</script>
